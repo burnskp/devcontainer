b2c4951f3962a2f922cd601776f1e8392fad122b
Kevin Burns <kevin@burnskp.com>
2025-12-02 14:58:46 -0600

    Make editorconfig linter happy

--- containers/dev/config/nvim/update.lua
+++ containers/dev/config/nvim/update.lua
@@ -21,12 +21,12 @@ local mason = lazy.plugins["mason.nvim"]
 local registry = require("mason-registry")
 
 local function contains(array, element)
-	for _, value in ipairs(array) do
-		if value == element then
-			return true
-		end
-	end
-	return false
+  for _, value in ipairs(array) do
+    if value == element then
+      return true
+    end
+  end
+  return false
 end
 
 -- Stuff below is for converting package aliases to names
@@ -34,18 +34,18 @@ local all_package_names = registry.get_all_package_names()
 local alias_to_name = {}
 
 for _, package_name in pairs(all_package_names) do
-	local aliases = registry.get_package_aliases(package_name)
-	if aliases then
-		for _, alias in pairs(aliases) do
-			alias_to_name[alias] = package_name
-		end
-		alias_to_name[package_name] = package_name
-	end
+  local aliases = registry.get_package_aliases(package_name)
+  if aliases then
+    for _, alias in pairs(aliases) do
+      alias_to_name[alias] = package_name
+    end
+    alias_to_name[package_name] = package_name
+  end
 end
 
 local already_installed = registry.get_installed_package_names()
 for idx, v in ipairs(already_installed) do
-	already_installed[idx] = alias_to_name[v]
+  already_installed[idx] = alias_to_name[v]
 end
 
 -- Load all packages into a list
@@ -54,34 +54,34 @@ local mason_stuff = {}
 -- lsp packages installed via nvim-lspconfig
 local servers = nvim_lsp._.cache.opts.servers
 for server_name, _ in pairs(servers) do
-	server_name = alias_to_name[server_name]
-	if not contains(already_installed, server_name) then
-		table.insert(mason_stuff, server_name)
-	end
+  server_name = alias_to_name[server_name]
+  if not contains(already_installed, server_name) then
+    table.insert(mason_stuff, server_name)
+  end
 end
 
 -- mason apps installed
 local mason_apps = mason._.cache.opts.ensure_installed
 for _, mason_app in pairs(mason_apps) do
-	mason_app = alias_to_name[mason_app]
-	if not contains(already_installed, mason_app) then
-		table.insert(mason_stuff, mason_app)
-	end
+  mason_app = alias_to_name[mason_app]
+  if not contains(already_installed, mason_app) then
+    table.insert(mason_stuff, mason_app)
+  end
 end
 
 local function install_mason_package(package_name)
-	print("Installing " .. package_name)
-	local ok, err = pcall(function()
-		vim.cmd("MasonInstall " .. package_name)
-	end)
-	if ok then
-		print("Installed " .. package_name)
-	else
-		print("Failed to install " .. package_name .. ": " .. tostring(err))
-	end
+  print("Installing " .. package_name)
+  local ok, err = pcall(function()
+    vim.cmd("MasonInstall " .. package_name)
+  end)
+  if ok then
+    print("Installed " .. package_name)
+  else
+    print("Failed to install " .. package_name .. ": " .. tostring(err))
+  end
 end
 
 -- install them one by one
 for _, v in ipairs(mason_stuff) do
-	install_mason_package(v)
+  install_mason_package(v)
 end
