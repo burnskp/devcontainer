72573d6abf122bcda58f047740f48fc0e32b1e79
Kevin Burns <kevin@burnskp.com>
2026-01-21 14:45:35 -0600

    Disable copilot ghost text

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -18,7 +18,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
     if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlineCompletion, bufnr) then
-      vim.lsp.inline_completion.enable(true, { bufnr = bufnr })
+      vim.lsp.inline_completion.enable(false, { bufnr = bufnr })
 
       vim.keymap.set(
         "i",
