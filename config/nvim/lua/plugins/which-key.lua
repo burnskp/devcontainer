188a086a272f8a1d980e293b565468219b8fbd24
Kevin Burns <kevin@burnskp.com>
2026-01-24 11:19:00 -0600

    Fix empty which-key descriptions

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -2,13 +2,16 @@ require("which-key").setup({
   preset = "helix",
   delay = 800,
   spec = {
+    { "<leader><leader>", group = "Smart Splits" },
     { "<leader>a", group = "Avante" },
     { "<leader>f", group = "Find" },
     { "<leader>g", group = "Git" },
+    { "<leader>h", group = "GitSigns" },
     { "<leader>k", group = "Sidekick" },
     { "<leader>l", group = "LSP" },
     { "<leader>n", group = "Notes" },
     { "<leader>u", group = "Toggle" },
+    { "<leader>uN", group = "Noice" },
     { "<leader>w", group = "Window" },
   },
 })
